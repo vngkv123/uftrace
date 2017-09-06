f8c186d7872b684337f68454110e5db385fa7aeb
Namhyung Kim <namhyung@gmail.com>
2017-09-06 22:31:46 +0900

    script: Add support for filtering
    Besides from the function filter, script might have its own list of
    interested functions.  If it has a list named "UFTRACE_FUNCS" at the
    top-level, it would be used as filters to run script for the specified
    functions only.
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

--- utils/script.c
+++ utils/script.c
@@ -8,6 +8,7 @@
 
 #include <unistd.h>
 #include "utils/script.h"
+#include "utils/list.h"
 
 
 /* This will be set by getenv("UFTRACE_SCRIPT"). */
@@ -17,6 +18,13 @@ char *script_str;
 script_uftrace_entry_t script_uftrace_entry;
 script_uftrace_exit_t script_uftrace_exit;
 script_uftrace_end_t script_uftrace_end;
+
+struct script_filter_item {
+	struct list_head list;
+	char *name;
+};
+
+static LIST_HEAD(filters);
 
 static enum script_type_t get_script_type(const char *str)
 {
@@ -30,6 +38,45 @@ static enum script_type_t get_script_type(const char *str)
 		return SCRIPT_PYTHON;
 
 	return SCRIPT_UNKNOWN;
+}
+
+void script_add_filter(char *func)
+{
+	struct script_filter_item *item;
+
+	if (func == NULL)
+		return;
+
+	item = xmalloc(sizeof(*item));
+
+	item->name = xstrdup(func);
+	list_add_tail(&item->list, &filters);
+}
+
+/* returns 1 on match - script should be run */
+int script_match_filter(char *func)
+{
+	struct script_filter_item *item;
+
+	/* special case: no filter */
+	if (list_empty(&filters))
+		return 1;
+
+	list_for_each_entry(item, &filters, list) {
+		if (!strcmp(item->name, func))
+			return 1;
+	}
+	return 0;
+}
+
+void script_finish_filter(void)
+{
+	struct script_filter_item *item, *tmp;
+
+	list_for_each_entry_safe(item, tmp, &filters, list) {
+		free(item->name);
+		free(item);
+	}
 }
 
 int script_init(char *script_pathname)
@@ -55,4 +102,9 @@ int script_init(char *script_pathname)
 		return -1;
 
 	return 0;
+}
+
+void script_finish(void)
+{
+	script_finish_filter();
 }
