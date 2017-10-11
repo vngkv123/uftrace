01880769c2c245b7b0ec10eb56e3403fec5eec5d
Namhyung Kim <namhyung@gmail.com>
2017-10-11 11:06:58 +0900

    dwarf: Add type info to dwarf argument spec
    The type info is chained (to track pointers and/or modifiers) in DWARF
    DIEs so it needs to see the base type.
    
    Signed-off-by: Namhyung Kim <namhyung@gmail.com>

--- utils/dwarf.c
+++ utils/dwarf.c
@@ -1,6 +1,7 @@
 #ifdef HAVE_LIBDW
 
 #include <stdio.h>
+#include <stdbool.h>
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
@@ -11,6 +12,7 @@
 #include "utils/utils.h"
 #include "utils/dwarf.h"
 #include "utils/symbol.h"
+#include "utils/filter.h"
 
 /* setup debug info from filename, return 0 for success */
 int setup_debug_info(const char *filename, struct debug_info *dinfo,
@@ -53,10 +55,115 @@ void release_debug_info(struct debug_info *dinfo)
 	dinfo->dw = NULL;
 }
 
+struct type_data {
+	enum uftrace_arg_format		fmt;
+	int				size;
+	int				pointer;
+};
+
+static int arg_type_cb(Dwarf_Attribute *attr, void *arg)
+{
+	unsigned aname = dwarf_whatattr(attr);
+	unsigned aform = dwarf_whatform(attr);
+	struct type_data *td = arg;
+	Dwarf_Die die;
+	Dwarf_Attribute type;
+	bool done = false;
+	const char *tname;
+
+	if (aname != DW_AT_type)
+		return DWARF_CB_OK;
+
+	while (!done) {
+		switch (aform) {
+		case DW_FORM_ref1:
+		case DW_FORM_ref2:
+		case DW_FORM_ref4:
+		case DW_FORM_ref8:
+		case DW_FORM_ref_udata:
+		case DW_FORM_ref_addr:
+		case DW_FORM_ref_sig8:
+		case DW_FORM_GNU_ref_alt:
+			dwarf_formref_die(attr, &die);
+			break;
+		default:
+			done = true;
+			continue;
+		}
+
+		switch (dwarf_tag(&die)) {
+		case DW_TAG_base_type:
+			tname = dwarf_diename(&die);
+			if (!strcmp(tname, "char") ||
+			    !strcmp(tname, "signed char")) {
+				if (td->pointer == 0)
+					td->fmt = ARG_FMT_CHAR;
+				else if (td->pointer == 1)
+					td->fmt = ARG_FMT_STR;
+			}
+			else if (!strcmp(tname, "float")) {
+				td->fmt = ARG_FMT_FLOAT;
+				td->size = 32;
+			}
+			else if (!strcmp(tname, "double")) {
+				td->fmt = ARG_FMT_FLOAT;
+				td->size = 64;
+			}
+			done = true;
+			continue;
+		case DW_TAG_pointer_type:
+		case DW_TAG_ptr_to_member_type:
+			td->pointer++;
+			/* fall through */
+		case DW_TAG_reference_type:
+		case DW_TAG_rvalue_reference_type:
+		case DW_TAG_array_type:
+		default:
+			if (!dwarf_hasattr(&die, DW_AT_type)) {
+				done = true;
+				continue;
+			}
+			break;
+		}
+
+		pr_dbg3("dwarf: %s (tag %d)\n", dwarf_diename(&die), dwarf_tag(&die));
+		dwarf_attr(&die, DW_AT_type, &type);
+		aform = dwarf_whatform(&type);
+		attr = &type;
+	}
+
+	return DWARF_CB_ABORT;
+}
+
 struct arg_data {
 	const char	*name;
+	int		fpidx;
 	char		*argspec;
 };
+
+static void add_type_info(char *spec, size_t len, Dwarf_Die *die, void *arg)
+{
+	struct arg_data *ad = arg;
+	struct type_data data = {
+		.fmt = ARG_FMT_AUTO,
+	};
+
+	dwarf_getattrs(die, arg_type_cb, &data, 0);
+
+	switch (data.fmt) {
+	case ARG_FMT_CHAR:
+		strcat(spec, "/c");
+		break;
+	case ARG_FMT_STR:
+		strcat(spec, "/s");
+		break;
+	case ARG_FMT_FLOAT:
+		snprintf(spec, len, "fparg%d/%d", ++ad->fpidx, data.size);
+		break;
+	default:
+		break;
+	}
+}
 
 static int get_argspec_cb(Dwarf_Die *die, void *data)
 {
@@ -75,9 +182,11 @@ static int get_argspec_cb(Dwarf_Die *die, void *data)
 	}
 
 	while (dwarf_tag(&arg) == DW_TAG_formal_parameter) {
-		char buf[16];
+		char buf[32];
 
 		snprintf(buf, sizeof(buf), "arg%d", idx++);
+		add_type_info(buf, sizeof(buf), &arg, ad);
+
 		ad->argspec = strjoin(ad->argspec, buf, ",");
 
 		if (dwarf_siblingof(&arg, &arg) != 0)
